f42b8fa1d0b1857e317bb236b06385e1b656a6a4
George Perez <pgeorge27@gmail.com>
2015-08-02 08:13:01 -0500

    Tu-Ingles First Commit >)
    Primer commit, app funcional para iOS y android

---
+++ platforms/ios/www/cordova_plugins.js
@@ -0,0 +1,35 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/org.apache.cordova.statusbar/www/statusbar.js",
+        "id": "org.apache.cordova.statusbar.statusbar",
+        "clobbers": [
+            "window.StatusBar"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.splashscreen/www/splashscreen.js",
+        "id": "org.apache.cordova.splashscreen.SplashScreen",
+        "clobbers": [
+            "navigator.splashscreen"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-admobpro/www/AdMob.js",
+        "id": "cordova-plugin-admobpro.AdMob",
+        "clobbers": [
+            "window.AdMob"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.0.0",
+    "org.apache.cordova.statusbar": "0.1.10",
+    "org.apache.cordova.splashscreen": "1.0.0",
+    "cordova-plugin-admobpro": "2.8.3",
+    "cordova-plugin-extension": "1.1.4"
+}
+// BOTTOM OF METADATA
+});
---
+++ www/js/admob.js
@@ -0,0 +1,72 @@
+    function onLoad() {
+        if(( /(ipad|iphone|ipod|android|windows phone)/i.test(navigator.userAgent) )) {
+            document.addEventListener('deviceready', initApp, false);
+        } else {
+            initApp();
+        }
+    }
+
+    var admobid = {};
+    if( /(android)/i.test(navigator.userAgent) ) {
+        admobid = {
+            banner: 'ca-app-pub-9365506700334816/4286268887',
+            interstitial: 'ca-app-pub-9365506700334816/6354550480'
+        };
+    } else if(/(ipod|iphone|ipad)/i.test(navigator.userAgent)) {
+        admobid = {
+            banner: 'ca-app-pub-9365506700334816/5325252883',
+            interstitial: 'ca-app-pub-9365506700334816/8278719285'
+        };
+    } else {
+        admobid = {
+            banner: 'ca-app-pub-9365506700334816/5325252883',
+            interstitial: 'ca-app-pub-9365506700334816/8278719285'
+        };
+    }
+
+    function initApp() {
+        if (! AdMob ) {
+            // alert( 'admob plugin not ready' );
+            return;
+        }
+        initAd();
+        // display the banner at startup
+        AdMob.createBanner( {adId:admobid.banner} );
+        AdMob.prepareInterstitial({adId:admobid.interstitial, autoShow:true});
+    }
+
+    function initAd(){
+        var defaultOptions = {
+            bannerId: admobid.banner,
+            interstitialId: admobid.interstitial,
+            adSize: 'SMART_BANNER',
+            // width: integer, // valid when set adSize 'CUSTOM'
+            // height: integer, // valid when set adSize 'CUSTOM'
+            position: AdMob.AD_POSITION.BOTTOM_CENTER,
+            offsetTopBar: true, // avoid overlapped by status bar, for iOS7+
+            bgColor: 'black', // color name, or '#RRGGBB'
+            overlap: false,
+            // x: integer,      // valid when set position to 0 / POS_XY
+            // y: integer,      // valid when set position to 0 / POS_XY
+            isTesting: true, // set to true, to receiving test ad for testing purpose
+            autoShow: true // auto show interstitial ad when loaded, set to false if prepare/show
+        };
+        AdMob.setOptions( defaultOptions );
+        registerAdEvents();
+    }
+
+    // optional, in case respond to events or handle error
+    function registerAdEvents() {
+        // new events, with variable to differentiate: adNetwork, adType, adEvent
+        document.addEventListener('onAdFailLoad', function(data){
+            // alert('error: ' + data.error +
+            //         ', reason: ' + data.reason +
+            //         ', adNetwork:' + data.adNetwork +
+            //         ', adType:' + data.adType +
+            //         ', adEvent:' + data.adEvent); // adType: 'banner' or 'interstitial'
+        });
+        document.addEventListener('onAdLoaded', function(data){});
+        document.addEventListener('onAdPresent', function(data){});
+        document.addEventListener('onAdLeaveApp', function(data){});
+        document.addEventListener('onAdDismiss', function(data){});
+    }
